511a4ced9969c95939985075d36d6fdc38d326ab
Joshua Collins <hello@joshuacollins.me>
2021-06-19 18:50:16 +1000

    Updates for Debian 10

--- linux.sh
+++ linux.sh
@@ -5,21 +5,24 @@
 ####    Built to make it easy to start fresh Kali VMs regularly.
 ####    Author: Joshua Collins
 
-base_packages="build-essential chromium curl firefox-esr ftp htop nmap slurm sshpass"
-dev_packages="ctags git gcc g++ make perl python3 python3-dev python3-pip shellcheck vim-runtime xterm"
+base_packages="build-essential chromium curl firefox-esr ftp htop nmap slurm ssh sshpass"
+dev_packages="ctags git gcc g++ make perl python-pip python3 python3-dev python3-pip shellcheck vim vim-runtime xterm"
+gui_packages="task-gnome-desktop"
 
 # Used for python2 and python3
 pip_packages="requests"
 perl_libraries="libdbi-perl"
+ruby_packages="ruby-full"
 tcl_packages="expect"
 
 novelty_packages="sysvbanner cowsay"
 
-vbox_packages="dkms virtualbox-guest-x11 virtualbox-guest-dkms virtualbox-guest-utils linux-headers-$(uname -r)"
+vbox_packages="build-essential dkms linux-headers-$(uname -r)"
 
 #What type of system is it?
 # kali | debian
 THIS_SYSTEM=""
+HAS_GUI=""
 
 # Sets THIS_SYSTEM variable. Currently detects kali and debian.
 detect_system()
@@ -43,6 +46,9 @@ unsupervised_initial_tasks()
     sudo apt-get update
     # Resolves some issues
     sudo apt-get -y dist-upgrade
+
+    #shhhhhhh
+    echo 'j' | sudo echo 'sudo'
 }
 
 # Ask a question, with a default (enter) answer of "Yes!"
@@ -66,20 +72,21 @@ ask_question()
 
 install_selected_packages()
 {
-    ask_question "Install basic-niceness packages?"
+    ask_question "Install basic packages?"
     if [ "$?" -eq 0 ]; then
         sudo apt-get -y install ${base_packages}
+    fi
+
+    ask_question "Install the GNOME GUI?"
+    if [ "$?" -eq 0 ]; then
+        sudo apt-get -y install ${gui_packages}
+        HAS_GUI="yes"
     fi
 
     ask_question "Install development packages (Python, C/C++, Perl, Vim, Make, Ctags)?"
     if [ "$?" -eq 0 ]; then
         sudo apt-get -y install ${dev_packages}
         sudo apt-get -y install ${perl_libraries}
-
-        ask_question "Install common Python2 libraries (using pip)?"
-        if [ "$?" -eq 0 ]; then
-            pip install ${pip_packages}
-        fi
 
         ask_question "Install common Python3 libraries (using pip)?"
         if [ "$?" -eq 0 ]; then
@@ -88,13 +95,18 @@ install_selected_packages()
 
         ask_question "Install node.js?"
         if [ "$?" -eq 0 ]; then
-            curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+            curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
             sudo apt-get install -y nodejs
         fi
 
         ask_question "Install TCL/Expect?"
         if [ "$?" -eq 0 ]; then
             sudo apt-get -y install ${tcl_packages}
+        fi
+
+        ask_question "Install Ruby?"
+        if [ "$?" -eq 0 ]; then
+            sudo apt-get -y install ${ruby_packages}
         fi
     fi
 
@@ -108,6 +120,11 @@ install_selected_packages()
 
 gnome_config()
 {
+    # Skip if there is no GUI installed
+    if [ "${HAS_GUI}" == "" ]; then
+        return
+    fi
+
     # Stop lock screen from appearing after you've been idle
     # Just hope gnome is installed, keep going if not
     gsettings set org.gnome.desktop.session idle-delay 0 || true
@@ -121,21 +138,18 @@ gnome_config()
 virtualbox_config()
 {
     # Because shipping a package with all the important features turned off is what they did!
-    VBoxClient --clipboard
-    VBoxClient --draganddrop
-    VBoxClient --display
-    VBoxClient --seamless
-    VBoxClient --vmsvga
+    sudo VBoxClient --clipboard
+    sudo VBoxClient --draganddrop
+    sudo VBoxClient --display
+    sudo VBoxClient --seamless
+    sudo VBoxClient --vmsvga
 
-    VBoxControl sharedfolder -automount
+    sudo VBoxControl sharedfolder -automount
 
     # Like, seriously!?! You thought I wanted my clock to stay stuck on installation day?
     VBoxService --enable-timesync
     VBoxService --enable-automount
 
-    # Ensure that it will be enabled on startup
-    systemctl start virtualbox-guest-utils
-    systemctl enable virtualbox-guest-utils
 }
 
 virtualbox_packages()
@@ -143,15 +157,21 @@ virtualbox_packages()
     ask_question "Install virtualbox extensions?"
 
     if [ "$?" -eq 0 ]; then
-        if [ "${THIS_SYSTEM}" == "debian" ]; then
-            echo deb http://ftp.debian.org/debian stretch-backports main contrib > /etc/apt/sources.list.d/stretch-backports.list
-            apt update
-        fi
-
         # Full integration packages for kali/ubuntu
         sudo apt-get -y install ${vbox_packages}
 
-        virtualbox_config
+        if [ -e '/media/cdrom' ]; then
+
+            sudo cp /media/cdrom/VBoxLinuxAdditions.run ./
+            sudo ./VBoxLinuxAdditions.run
+
+            virtualbox_config
+
+            sudo rm ./VBoxLinuxAdditions.run
+
+        else
+            echo "Couldn't find VirtualBox Guest Additions CDROM"
+        fi
     fi
 }
 
@@ -180,19 +200,26 @@ secure_ssh()
         echo "${trusted_key}" >> ~/.ssh/authorized_keys
     fi
 
+    #TODO: Determine effective user - if running as sudo in a system without root, we try to write to
+    # non-existent directories.
+
     # Set appropriate permissions for the ~/.ssh directory
     chmod 700 ~/.ssh
     chmod 600 ~/.ssh/authorized_keys || true
 
-    # Clear offending lines, append at the end
-    sed -e 's/^PermitRootLogin.*//g' \
-        -e 's/^PasswordAuthentication.*//g' \
-        < /etc/ssh/sshd_config > /etc/ssh/sshd_config.tmp
+    if [ -e "/etc/ssh/sshd_config" ]; then
+        # Clear offending lines, append at the end
+        sudo sed -e 's/^PermitRootLogin.*//g' \
+            -e 's/^PasswordAuthentication.*//g' \
+            < /etc/ssh/sshd_config > /etc/ssh/sshd_config.tmp
 
-    # Append the settings we want to set
-    echo -e "# Added by $0\nPermitRootLogin no\nPasswordAuthentication no\n" >> /etc/ssh/sshd_config.tmp
+        # Append the settings we want to set
+        sudo echo -e "# Added by $0\nPermitRootLogin no\nPasswordAuthentication no\n" >> /etc/ssh/sshd_config.tmp
 
-    mv /etc/ssh/sshd_config.tmp /etc/ssh/sshd_config
+        sudo mv /etc/ssh/sshd_config.tmp /etc/ssh/sshd_config
+    else
+        echo "SSH server not installed, skipping"
+    fi
 }
 
 bash_profile()
@@ -235,6 +262,12 @@ msf_init()
     fi
 }
 
+clean_up()
+{
+    echo "Removing packages no longer required"
+    apt autoremove
+}
+
 
 main()
 {
@@ -249,6 +282,7 @@ main()
     gnome_config
     bash_profile
     vim_setup
+    clean_up
 
     ask_question "Reboot now?"
     if [ "$?" -eq 0 ]; then
